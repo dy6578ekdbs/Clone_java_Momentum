2fe88e968e35b70a3a97c6752b25a1a047cdb4a9
[dayun] <[dy6578ekdbs@gmail.com]>
2021-08-19 22:51:13 +0900

    210819

--- app2.js
+++ app2.js
@@ -1,15 +1,33 @@
-const title = document.querySelector("h1");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-console.log(title);
+const HIDDEN_CLASSNAME = 'hidden';
+const NAME_KEY = "name";
 
-function handTitleClick(){
-  
-
-    title.classList.toggle("clicked");
-
+function onLoginSubmit(text){
+    text.preventDefault();
+    const name = loginInput.value;
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    console.log(name);
+    localStorage.setItem(NAME_KEY, name);
+    paintGreeting(name)
 }
 
+function paintGreeting(name){
+    greeting.innerText = `Hello ${name}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
 
-title.addEventListener("click",  handTitleClick);
+loginForm.addEventListener("submit", onLoginSubmit);
+
+const saveName = localStorage.getItem(NAME_KEY);
+
+if(saveName === null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
 
 
+} else {
+    paintGreeting(saveName);
+}
